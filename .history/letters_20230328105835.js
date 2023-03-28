47e0f2e833775d592fa1b429aafaa0e7d34056bb
DevLaukey <devlaukeymwaura@gmail.com>
2023-03-28 11:17:41 +0300

    Reading values

---
+++ .history/letters_20230328105835.js
@@ -0,0 +1,36 @@
+
+var letterCombinations = function (digits) {
+
+    let output_arr = [];
+
+    let length = digits.length;
+
+
+
+};
+
+function mapper(digits) {
+    let strings = [];
+
+    digits.map((digit) => {
+        switch (digit) {
+            case digit == 2:
+                strings = [a, b, c];
+            case digit == 3:
+                strings = [d,e,f];
+            case digit == 4:
+                strings = [g, h, i];
+            case digit == 5:
+                strings = [j, k, l];
+            case digit == 6:
+                strings = [m, n, o];
+            case digit == 7:
+                strings = [p, q, r, s];
+            case digit == 8:
+                strings = [t, u, v];
+            case digit == 9:
+                strings = [w, x, y, z];
+
+        }
+    })
+}
---
+++ .history/letters_20230328111537.js
@@ -0,0 +1,55 @@
+
+
+letterCombinations("23");
+var letterCombinations = function (digits) {
+
+    let output_arr = [];
+
+    let length = digits.length;
+
+    if (length == 0) {
+        return output_arr;
+    }
+    else {
+        let strings = [];
+        for (let i = 0; i < length; i++){
+            one_arr = mapper(digits.split(','));
+            strings.push(one_arr);
+
+        }
+
+
+console.log(strings);
+      
+    }
+
+
+
+};
+
+function mapper(digits) {
+    let strings = [];
+
+    digits.map((digit) => {
+        switch (digit) {
+            case digit == 2:
+                strings = [a, b, c];
+            case digit == 3:
+                strings = [d,e,f];
+            case digit == 4:
+                strings = [g, h, i];
+            case digit == 5:
+                strings = [j, k, l];
+            case digit == 6:
+                strings = [m, n, o];
+            case digit == 7:
+                strings = [p, q, r, s];
+            case digit == 8:
+                strings = [t, u, v];
+            case digit == 9:
+                strings = [w, x, y, z];
+        }
+    })
+
+    return strings;
+}
